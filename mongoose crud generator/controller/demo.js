8841a50f5ae21522ee43080c3db160ea325d3594
bhargav-vaghela <bhargavvaghela1856@gmail.com>
2026-09-23 18:43:53 +0530

    mongoose update

--- mongoose crud generator/controller/demo.js
+++ mongoose crud generator/controller/demo.js
@@ -4,15 +4,36 @@ exports.viewpage = async(req,res)=>{
 
     const allData = await USER.find()
 
-    res.render('demo',{allData})
+    res.render('demo',{allData,editData:null})
     
 }
 
-exports.createData = async(req,res)=>{
+exports.dataCreate = async(req,res)=>{
 
     const data = req.body
 
-    await USER.create(data)
+    if(data.id != "")
+    {
+        await USER.findByIdAndUpdate(data.id,{$set:data})
+    }
+    else
+    {
+        await USER.create(data)
+    }   
 
     res.redirect('/')
+}
+
+exports.deleteData = async(req,res)=>{
+    const deleteid = req.params.id
+    console.log(deleteid)
+    await USER.findByIdAndDelete(deleteid)
+    res.redirect('/')
+}
+
+exports.updateDAta = async(req,res)=>{
+    const editid = req.params.id
+    const editData = await USER.findById(editid)
+    const allData = await USER.find()
+    res.render('demo',{editData,allData})
 }
--- mongoose crud generator/routes/demo.js
+++ mongoose crud generator/routes/demo.js
@@ -4,6 +4,8 @@ const router = express.Router()
 const DC = require('../controller/demo')
 
 router.get('/',DC.viewpage)
-router.post('/createData',DC.createData)
+router.post('/createData',DC.dataCreate)
+router.get('/deleteData/:id',DC.deleteData)
+router.get('/editData/:id',DC.updateDAta)
 
 module.exports = router
